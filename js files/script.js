1e88e1428606ac942afba1f91b59a40fda402914
Thomas <90970828+ThomasKKA@users.noreply.github.com>
2026-07-20 21:43:40 -0400

    Add files via upload

---
+++ js files/script.js
@@ -0,0 +1,13 @@
+﻿const myButton = document.getElementById("myButton");
+if (myButton) {
+    myButton.addEventListener("click", function () {
+        window.location.href = "NextPage.html";
+    });
+}
+
+const goBackButton = document.getElementById("goBackButton");
+if (goBackButton) {
+    goBackButton.addEventListener("click", function () {
+        window.location.href = "index.html";
+    });
+}
